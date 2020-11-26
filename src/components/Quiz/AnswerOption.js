64db0c1b22c36ce52a0f7e79972f5552bef97baf
HieuPhamBa <45411070+HieuPhamBa@users.noreply.github.com>
2020-11-26 11:38:30 +0700

    Completed lesson 16

---
+++ src/components/Quiz/AnswerOption.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AnswerOption = function (props) {
+    return (
+        <div class="option">
+            {props.text}
+        </div>
+    )
+}
+
+export default AnswerOption
